611d8c09f6c90956e43b756c18afab183b0691bc
DanielUribeDGSan <daniel.uribe.garcia07@gmail.com>
2023-03-14 00:03:30 -0600

    fix: resolver errores de build

--- src/pages/portfolio-details/[id].jsx
+++ src/pages/portfolio-details/[id].jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import SEO from '../../components/seo';
-import { Wrapper } from '../../layout';
-import PortfolioDetailsMain from '../../components/portfolios/portfolio-details';
-import { portfolio_data } from '../../data';
-import { useRouter } from 'next/router';
-
+import React from "react";
+import SEO from "../../components/seo";
+import { Wrapper } from "../../layout";
+import PortfolioDetailsMain from "../../components/portfolios/portfolio-details";
+import { portfolio_data } from "../../data";
+import { useRouter } from "next/router";
 
 const PortfolioDynamicDetails = () => {
   const router = useRouter();
-  const {id} = router.query;
-  const case_item = portfolio_data.find(item => Number(item.id) === Number(id))
+  const { id } = router.query;
+  const case_item = portfolio_data.find(
+    (item) => Number(item.id) === Number(id)
+  );
   return (
     <Wrapper>
-      <SEO pageTitle={'Portfolio Details'} />
+      <SEO pageTitle={"Portfolio Details"} />
       <PortfolioDetailsMain case_item={case_item} />
     </Wrapper>
   );
@@ -22,6 +23,6 @@ export default PortfolioDynamicDetails;
 
 export async function getServerSideProps(context) {
   return {
-      props: {},
+    props: {},
   };
 }
--- src/pages/portfolio-details/index.jsx
+++ src/pages/portfolio-details/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import SEO from '../../components/seo';
-import { Wrapper } from '../../layout';
-import PortfolioDetailsMain from '../../components/portfolios/portfolio-details';
-import { portfolio_data } from '../../data';
+import React from "react";
+import SEO from "../../components/seo";
+import { Wrapper } from "../../layout";
+import PortfolioDetailsMain from "../../components/portfolios/portfolio-details";
+import { portfolio_data } from "../../data";
 
-const case_item = portfolio_data[11]
+const case_item = portfolio_data[11];
 
 const index = () => {
   return (
     <Wrapper>
-      <SEO pageTitle={'Portfolio Details'} />
+      <SEO pageTitle={"Portfolio Details"} />
       <PortfolioDetailsMain case_item={case_item} />
     </Wrapper>
   );
